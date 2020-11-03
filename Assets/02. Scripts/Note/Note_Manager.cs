94a1ae655fbd7a681e6f56735eb7523d5313ee22
SunhuchuY <59392508+SunhuchuY@users.noreply.github.com>
2020-11-03 22:11:46 +0900

    2020 - 11 - 03 오후 10:10
    나중에 추가에 따라 등급 판정 시스템을 만들있게 코드를 작성해뒀음.
    현재는 등급 두개(미쓰,  굿)만 존재하며 콘솔창으로 무슨 등급이 판정나는지 확인가능

---
+++ Assets/02. Scripts/Note/Note_Manager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note_Manager : MonoBehaviour
+{
+    public int bpm = 0; // 비트 펄 미닛의 약자
+    double currentTime = 0d;
+
+    public Transform noteApped_tr; // 노트가 생성 될 위치
+    public GameObject note_prefeb; // 노트 프리펩
+
+    timing_Manager timing_Manager;
+
+    private void Start()
+    {
+        timing_Manager = GetComponent<timing_Manager>();
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if(currentTime >= 60d / bpm)
+        {
+            GameObject temp_note = Instantiate(note_prefeb, noteApped_tr.position, Quaternion.identity);
+            temp_note.transform.SetParent(this.transform);
+            timing_Manager.boxNodeList.Add(temp_note);
+            currentTime -= 60d / bpm; // 시간 손실 줄이기 위해
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Note"))
+        {
+            timing_Manager.boxNodeList.Remove(col.gameObject);
+            Destroy(col.gameObject);
+        }
+    }
+}
